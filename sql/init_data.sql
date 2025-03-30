35b7ce3c510f1afc30c4c72496694cd534eb4e75
Adriel <adrielmolina99@gmail.com>
2025-03-30 17:36:32 +0800

    init data added to commit

---
+++ sql/init_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO accounts 
+-- TODO change the password into hashed versions and update other personal info
+
+VALUES 
+	(NULL, 'adriel', 'adriel123', 'adrielmolina99@gmail.com', 'adriel', 'del rosario', 'panganiban', 'NULL', '2000-12-30', '09151095728', '2025-03-30', 'Chapter', 'admin', 'approved', '2025-03-30'),
+	(NULL, 'louis', 'louis123', 'sanorjo.louis111@gmail.com', 'louis', 'dano', 'sanorjo', 'NULL', '2000-12-30', '09151095728', '2025-03-30', 'Chapter', 'admin', 'approved', '2025-03-30'),
+	(NULL, 'abby', 'abby123', 'abegailmontejo9@gmail.com', 'abegail', 'jane', 'montejo', 'NULL', '2000-12-30', '09151095728', '2025-03-30', 'Chapter', 'admin', 'approved', '2025-03-30'),
+	(NULL, 'jb', 'jb123', 'johnbenedictcustodio8@gmail.com', 'john', 'benedict', 'custodio', 'NULL', '2000-12-30', '09151095728', '2025-03-30', 'Chapter', 'user', 'approved', '2025-03-30'),
+	(NULL, 'josh', 'josh123', 'crisostomojosh23@gmail.com', 'josh', 'gobres', 'crisostomo', 'NULL', '2000-12-30', '09151095728', '2025-03-30', 'Chapter', 'user', 'approved', '2025-03-30')
+;
