f2e452119ede72ad8a062b57b3e6d2bece68d3ed
Adriel <adrielmolina99@gmail.com>
2025-10-19 04:56:15 +0800

    added svg files for later

--- flask_server.py
+++ flask_server.py
@@ -12,6 +12,8 @@ from functools import wraps
 server = Flask(__name__)
 server.jinja_env.auto_reload = True
 server.secret_key = os.urandom(24)
+
+# CACHE CONTROL FOR STATIC FILES
 server.config['SEND_FILE_MAX_AGE_DEFAULT'] = 31536000
 
 login_manager = LoginManager()
